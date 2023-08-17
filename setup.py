2813d2343aed35bab88dc027b47c65afec234b90
Ortiz <henrique.ortiz@inteliger.com.br>
2023-08-17 16:10:25 -0300

    version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FileSchemaValidator',
-    version='0.0.1',
+    version='0.0.2',
     description='This module provides a powerful and extensible solution for validating CSV files against customizable schemas in Python',
     author='Henrique Ortiz',
     author_email='henriqueortiz.1@gmail.com',
