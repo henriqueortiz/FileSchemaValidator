ce322bb7673e22472a322b20fa442474048e3fc8
Ortiz <henrique.ortiz@inteliger.com.br>
2023-08-17 16:12:40 -0300

    version 0.0.2

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description='This module provides a powerful and extensible solution for validating CSV files against customizable schemas in Python',
     author='Henrique Ortiz',
     author_email='henriqueortiz.1@gmail.com',
-    url='https://github.com/henriqueortiz/FileSchemaValidator'
+    url='https://github.com/henriqueortiz/FileSchemaValidator',
     packages=find_packages(),
 )
